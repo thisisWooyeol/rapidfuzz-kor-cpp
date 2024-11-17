8979239ca0d1cbfd3ea3d5239f9f8f8d6a2478a0
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-11-18 00:12:22 +0900

    🎨 Use namespace for test script

--- rapidfuzz/utils-hangul/standardPronunciation/rules/transform9And10And11th.test.cpp
+++ rapidfuzz/utils-hangul/standardPronunciation/rules/transform9And10And11th.test.cpp
@@ -9,14 +9,16 @@ namespace Hangul {
 namespace StandardPronunciation {
 namespace Rules {
 
+using namespace _Internal;
+using namespace DisassembleCompleteCharacter;
+
 TEST_CASE("transform9And10And11th", "[transform9And10And11th]")
 {
     SECTION("9항 - 받침 \"ㄲ, ㅋ\" / \"ㅅ, ㅆ, ㅈ, ㅊ, ㅌ\" / \"ㅍ\"은 어말 또는 자음 앞에서 각각 대표음 "
             "\"ㄱ, ㄷ, ㅂ\"으로 발음한다.")
     {
-        const auto& current =
-            _Internal::defined(DisassembleCompleteCharacter::disassembleCompleteCharacter(L"닦"));
-        const auto& next = DisassembleCompleteCharacter::disassembleCompleteCharacter(L"다");
+        const auto& current = defined(disassembleCompleteCharacter(L"닦"));
+        const auto& next = disassembleCompleteCharacter(L"다");
 
         const auto& result = transform9And10And11th(current, next);
         REQUIRE(result.current.choseong == L"ㄷ");
@@ -27,9 +29,8 @@ TEST_CASE("transform9And10And11th", "[transform9And10And11th]")
     SECTION("10항 - 겹받침 \"ㄳ\" / \"ㄵ\" / \"ㄼ, ㄽ, ㄾ\" / \"ㅄ\"은 어말 또는 자음 앞에서 각각 \"ㄱ, ㄴ, "
             "ㄹ, ㅂ\"으로 발음한다.")
     {
-        const auto& current =
-            _Internal::defined(DisassembleCompleteCharacter::disassembleCompleteCharacter(L"앉"));
-        const auto& next = DisassembleCompleteCharacter::disassembleCompleteCharacter(L"다");
+        const auto& current = defined(disassembleCompleteCharacter(L"앉"));
+        const auto& next = disassembleCompleteCharacter(L"다");
 
         const auto& result = transform9And10And11th(current, next);
         REQUIRE(result.current.choseong == L"ㅇ");
@@ -40,9 +41,8 @@ TEST_CASE("transform9And10And11th", "[transform9And10And11th]")
     SECTION(
         "11항 - 겹받침 \"ㄺ\" / \"ㄻ\" / \"ㄿ\"은 어말 또는 자음 앞에서 각각 \"ㄱ, ㅁ, ㅂ\"으로 발음한다.")
     {
-        const auto& current =
-            _Internal::defined(DisassembleCompleteCharacter::disassembleCompleteCharacter(L"흙"));
-        const auto& next = DisassembleCompleteCharacter::disassembleCompleteCharacter(L"과");
+        const auto& current = defined(disassembleCompleteCharacter(L"흙"));
+        const auto& next = disassembleCompleteCharacter(L"과");
 
         const auto& result = transform9And10And11th(current, next);
         REQUIRE(result.current.choseong == L"ㅎ");
