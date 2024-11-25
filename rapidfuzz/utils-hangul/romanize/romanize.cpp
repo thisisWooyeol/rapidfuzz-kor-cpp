66bc1efdfd1901e69cede50abd500a6155a44566
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-11-25 17:31:56 +0900

    🐛 Fix wrong implementation of romanize

--- rapidfuzz/utils-hangul/romanize/romanize.cpp
+++ rapidfuzz/utils-hangul/romanize/romanize.cpp
@@ -12,9 +12,10 @@ namespace Utils {
 namespace Hangul {
 namespace Romanize {
 
-std::wstring romanizeSyllableHangul(const std::wstring& syllable,
-                                    const std::vector<std::wstring>& arrayHangul, size_t index)
+std::wstring romanizeSyllableHangul(const std::vector<std::wstring>& arrayHangul, size_t index)
 {
+    std::wstring syllable = arrayHangul[index];
+
     // Check if the syllable is a Hangul character
     if (_Internal::isHangulCharacter(syllable)) {
         auto disassemble =
@@ -26,7 +27,7 @@ std::wstring romanizeSyllableHangul(const std::wstring& syllable,
 
         // Extract components
         std::wstring choseong = disassemble.value().choseong;
-        std::wstring jungseong = disassemble.value().jungseong;
+        std::wstring jungseong = Assemble::assemble(std::vector{disassemble.value().jungseong});
         std::wstring jongseong = disassemble.value().jongseong;
 
         // Map components to Roman letters
@@ -35,21 +36,21 @@ std::wstring romanizeSyllableHangul(const std::wstring& syllable,
         std::wstring romanJongseong = L"";
 
         // Map Choseong
-        auto itChoseong = CHOSEONG_ALPHABET_PRONUNCIATION.find(choseong[0]);
-        if (itChoseong != CHOSEONG_ALPHABET_PRONUNCIATION.end()) {
+        auto itChoseong = 초성_알파벳_발음.find(choseong[0]);
+        if (itChoseong != 초성_알파벳_발음.end()) {
             romanChoseong = itChoseong->second;
         }
 
         // Map Jungseong
-        auto itJungseong = JUNGSEONG_ALPHABET_PRONUNCIATION.find(jungseong[0]);
-        if (itJungseong != JUNGSEONG_ALPHABET_PRONUNCIATION.end()) {
+        auto itJungseong = 중성_알파벳_발음.find(jungseong[0]);
+        if (itJungseong != 중성_알파벳_발음.end()) {
             romanJungseong = itJungseong->second;
         }
 
         // Map Jongseong
         if (!jongseong.empty()) {
-            auto itJongseong = JONGSEONG_ALPHABET_PRONUNCIATION.find(jongseong);
-            if (itJongseong != JONGSEONG_ALPHABET_PRONUNCIATION.end()) {
+            auto itJongseong = 종성_알파벳_발음.find(jongseong);
+            if (itJongseong != 종성_알파벳_발음.end()) {
                 romanJongseong = itJongseong->second;
             }
         }
@@ -70,15 +71,15 @@ std::wstring romanizeSyllableHangul(const std::wstring& syllable,
     }
 
     // If syllable is a vowel
-    auto itVowel = JUNGSEONG_ALPHABET_PRONUNCIATION.find(syllable[0]);
-    if (itVowel != JUNGSEONG_ALPHABET_PRONUNCIATION.end()) {
+    auto itVowel = 중성_알파벳_발음.find(syllable[0]);
+    if (itVowel != 중성_알파벳_발음.end()) {
         return itVowel->second;
     }
 
     // If syllable can be a Choseong
     if (CanBe::canBeChoseong(std::wstring(1, syllable[0]))) {
-        auto itChoseong = CHOSEONG_ALPHABET_PRONUNCIATION.find(syllable[0]);
-        if (itChoseong != CHOSEONG_ALPHABET_PRONUNCIATION.end()) {
+        auto itChoseong = 초성_알파벳_발음.find(syllable[0]);
+        if (itChoseong != 초성_알파벳_발음.end()) {
             return itChoseong->second;
         }
     }
@@ -93,10 +94,13 @@ std::wstring romanize(const std::wstring& hangul)
     std::wstring standardizedHangul = StandardPronunciation::standardizePronunciation(hangul, options);
 
     std::wstring romanized = L"";
-
+    std::vector<std::wstring> arrayHangul{};
     for (size_t i = 0; i < standardizedHangul.length(); ++i) {
-        std::wstring syllable = std::wstring(1, standardizedHangul[i]);
-        romanized += romanizeSyllableHangul(syllable, std::vector<std::wstring>{standardizedHangul}, i);
+        arrayHangul.push_back(std::wstring(1, standardizedHangul[i]));
+    }
+
+    for (size_t i = 0; i < arrayHangul.size(); ++i) {
+        romanized += romanizeSyllableHangul(arrayHangul, i);
     }
 
     return romanized;
